fd53ecc0164b90d5db82d1e909471ca76b417d9d
Andrés66666 <cya2024773@est.univalle.edu>
2025-08-26 16:37:13 -0400

    Primer commit

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,21 +3,22 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { StorageService } from '../services/Storage.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const platformId = inject(PLATFORM_ID);
+  const storage = inject(StorageService); // 👈 inyección del servicio
 
   if (isPlatformBrowser(platformId)) {
-    const token = localStorage.getItem('access_token');
+    const token = storage.getItem('access_token'); // 👈 uso del servicio
     if (token) {
       return true;
     } else {
-      router.navigate(['/app-login']);
+      router.navigate(['/login']); // 👈 corrige la ruta de login
       return false;
     }
   } else {
-    // Si se está ejecutando en el servidor, no hacer nada (o devolver false)
     return false;
   }
 };
