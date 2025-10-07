115f4f456ce8e7471d206f1c28af7afc70193bd6
Andrés66666 <cya2024773@est.univalle.edu>
2025-10-06 20:54:05 -0400

    Fin

--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -22,7 +22,9 @@ import {
   providedIn: 'root',
 })
 export class ServiciosService {
-  private apiUrl = environment.apiUrl;
+/*   private apiUrl = environment.apiUrl; */
+  /* private apiUrl = 'http://localhost:8000/api/'; */
+  private apiUrl = 'https://backendecuacion.onrender.com/api/';
   constructor(private http: HttpClient) {
      console.log('API URL en servicio:', this.apiUrl);
   }
